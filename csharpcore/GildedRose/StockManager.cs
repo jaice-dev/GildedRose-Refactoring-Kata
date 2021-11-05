6806c298ee37c11a7efbcc2d3dc1fb13290f0980
Jordan <jordan.d.carlton@gmail.com>
2021-11-05 16:11:11 +0000

    Refactored all code, about to fix conjured

---
+++ csharpcore/GildedRose/StockManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using GildedRoseKata;
+
+namespace GildedRose
+{
+    public class StockManager
+    {
+        IList<Item> Items;
+        public StockManager(IList<Item> items)
+        {
+            Items = items;
+        }
+
+        public void UpdateQuality()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                switch (Items[i].Name)
+                {
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        UpdateBackstagePass(i);
+                        break;
+                    case "Aged Brie":
+                        IncreaseQualityBy1(i);
+                        break;
+                    default:
+                        DecreaseQualityBy1(i);
+                        break;
+                }
+                
+                UpdateSellIn(i);
+
+                if (Items[i].SellIn < 0)
+                {
+                    OutOfDate(i);
+                }
+            }
+        }
+
+        private void UpdateSellIn(int i)
+        {
+            if (Items[i].Name != "Sulfuras, Hand of Ragnaros") Items[i].SellIn -= 1;
+        }
+
+        private void UpdateBackstagePass(int i)
+        {
+            IncreaseQualityBy1(i);
+
+            if (Items[i].SellIn < 11) IncreaseQualityBy1(i);
+
+            if (Items[i].SellIn < 6) IncreaseQualityBy1(i);
+        }
+
+        private void OutOfDate(int i)
+        {
+            switch (Items[i].Name)
+            {
+                case "Aged Brie":
+                    IncreaseQualityBy1(i);
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    Items[i].Quality = 0;
+                    break;
+                default:
+                    DecreaseQualityBy1(i);
+                    break;
+            }
+        }
+
+        private void IncreaseQualityBy1(int i)
+        {
+            if (Items[i].Quality < 50) Items[i].Quality += 1;
+        }
+
+        private void DecreaseQualityBy1(int i)
+        {
+            if (Items[i].Quality > 0 && Items[i].Name != "Sulfuras, Hand of Ragnaros") Items[i].Quality -= 1;
+        }
+    }
+}
--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -1,18 +1,139 @@
 ﻿using Xunit;
 using System.Collections.Generic;
+using ApprovalUtilities.Utilities;
 using GildedRoseKata;
 
 namespace GildedRoseTests
 {
     public class GildedRoseTest
     {
-        [Fact]
-        public void foo()
+        private static IList<Item> GenerateItems()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
+            return new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 49
+                },
+                // this conjured item does not work properly yet
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+        }
+
+        [Fact]
+        public void Foo()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose.StockManager app = new GildedRose.StockManager(items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            Assert.Equal("foo", items[0].Name);
+        }
+        [Fact]
+        public void CheckQualityDegradesBy1InIfInDate()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 10 } };
+            GildedRose.StockManager app = new GildedRose.StockManager(items);
+            app.UpdateQuality();
+            Assert.Equal(9, items[0].Quality);
+        }
+        [Fact]
+        public void CheckQualityDegradesTwiceAsFastAfterSellDate()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 10 } };
+            GildedRose.StockManager app = new GildedRose.StockManager(items);
+            app.UpdateQuality();
+            Assert.Equal(8, items[0].Quality);
+        }
+        
+        [Fact]
+        public void QualityCannotBeNegative()
+        {
+            IList<Item> items = GenerateItems();
+            GildedRose.StockManager app = new GildedRose.StockManager(items);
+            for (var i = 0; i < 100; i++)
+            {
+                app.UpdateQuality();
+            }
+            Assert.All(items, item => Assert.True(!(item.Quality < 0)));
+        }
+        
+        [Fact]
+        public void AgedBrieIncreasesInQualityWithTime()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 } };
+            GildedRose.StockManager app = new GildedRose.StockManager(items);
+            for (var i = 0; i < 100; i++)
+            {
+                app.UpdateQuality();
+            }
+            Assert.True(items[0].Quality > 0);
+        }
+        
+        [Fact]
+        public void QualityIsNeverMoreThan50UnlessRagnaros()
+        {
+            IList<Item> items = GenerateItems();
+            GildedRose.StockManager app = new GildedRose.StockManager(items);
+            for (var i = 0; i < 100; i++)
+            {
+                app.UpdateQuality();
+            }
+            Assert.All(items, item => Assert.True(
+                item.Quality <= 50 | (item.Name == "Sulfuras, Hand of Ragnaros" && item.Quality == 80)
+                ));
+        }
+        
+        [Fact]
+        public void SulfurasAlwaysHasQualityOf80()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 } };
+            GildedRose.StockManager app = new GildedRose.StockManager(items);
+            for (var i = 0; i < 100; i++)
+            {
+                app.UpdateQuality();
+            }
+            Assert.All(items, item => Assert.True(item.Quality == 80));
+        }
+        
+        [Fact]
+        public void ConjuredItemsDegradeInQualityTwiceAsFast()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 } };
+            GildedRose.StockManager app = new GildedRose.StockManager(items);
+            
+            app.UpdateQuality();
+
+            Assert.Equal(4 , items[0].Quality);
+        }
+        
+        [Fact]
+        public void OutOfDateConjuredItemsDegradeInQuality4TimesAsFast()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 0, Quality = 10 } };
+            GildedRose.StockManager app = new GildedRose.StockManager(items);
+            
+            app.UpdateQuality();
+
+            Assert.Equal(6 , items[0].Quality);
         }
     }
 }
