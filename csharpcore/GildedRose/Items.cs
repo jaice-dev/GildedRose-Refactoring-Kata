9e0498f59e233437b02d60bd6d08b3c2b9b3e55f
Jordan <jordan.d.carlton@gmail.com>
2021-11-08 11:13:12 +0000

    Add Item classes

---
+++ csharpcore/GildedRose/Items.cs
@@ -0,0 +1,61 @@
+﻿using GildedRoseKata;
+
+namespace GildedRose
+{
+    public class Normal : Item
+    {
+        public void Update()
+        {
+            SellIn -= 1;
+            Quality -= 1;
+            if (SellIn < 0) Quality -= 1;
+            LimitMinimumQuality();
+        }
+
+        public void LimitMinimumQuality()
+        {
+            if (Quality < 0) Quality = 0;
+        }
+
+        public string Log()
+        {
+            return Name + ", " + SellIn + ", " + Quality;
+        }
+    }
+
+    public class AgedBrie : Normal
+    {
+        public new void Update()
+        {
+            SellIn -= 1;
+            Quality += 1;
+            if (SellIn < 0) Quality += 1;
+            LimitMinimumQuality();
+        }
+    }
+
+    public class Backstage : Normal
+    {
+        public new void Update()
+        {
+            SellIn -= 1;
+            if (SellIn < 0)
+            {
+                Quality = 0;
+                return;
+            }
+
+            Quality += 1;
+            if (SellIn < 10) Quality += 1;
+            if (SellIn < 5) Quality += 1;
+            LimitMinimumQuality();
+        }
+    }
+
+    public class Sulfuras : Normal
+    {
+        public new void Update()
+        {
+        }
+    }
+}
